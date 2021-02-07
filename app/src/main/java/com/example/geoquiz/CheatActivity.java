f863a0bbe949bfa1fa31845e6e9a8c3c0e741aa3
Pavel Vlasenko <p.vlasenko.sky7games@gmail.com>
2021-02-07 12:59:13 +0200

    cheating check if device rotate is fixed

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -3,15 +3,20 @@ package com.example.geoquiz;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String TAG = "CheatActivity";
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown";
+    private static final String IS_ANSWER_SHOWN = "isAnswerShownKey";
+    private static boolean isAnswerShown;
     private boolean mAnswerIsTrue;
 
     private TextView mAnswerTextView;
@@ -19,7 +24,16 @@ public class CheatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_cheat);
+
+        if (savedInstanceState != null) {
+            isAnswerShown = savedInstanceState.getBoolean(IS_ANSWER_SHOWN);
+        }
+        if(isAnswerShown){
+            setAnswerShownResult();
+        }
+        Log.d(TAG, "isAnswerShown = " + isAnswerShown);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
@@ -33,18 +47,29 @@ public class CheatActivity extends AppCompatActivity {
     }
 
     public static Intent newIntent(Context context, boolean answerIsTrue) {
+        Log.d(TAG, "newIntent()");
         Intent intent = new Intent(context, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return intent;
     }
 
     public static boolean wasAnswerShown(Intent resultIntent){
+        Log.d(TAG, "wasAnswerShown()");
         return resultIntent.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 
     private void setAnswerShownResult() {
+        Log.d(TAG, "setAnswerShowResult()");
         Intent dataIntent = new Intent();
         dataIntent.putExtra(EXTRA_ANSWER_SHOWN, true);
         setResult(RESULT_OK, dataIntent);
+        isAnswerShown = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSavedInstanceState(Bundle)");
+        outState.putBoolean(IS_ANSWER_SHOWN, isAnswerShown);
     }
 }
